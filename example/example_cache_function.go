5942b45286f8ff2ff1493f27896d0d26f1ec16da
Morteza Dehghani <mrtdeh@localhost.localdomain>
2022-03-27 09:26:54 +0430

    first commit

---
+++ example/example_cache_function.go
@@ -0,0 +1,38 @@
+package example
+
+import (
+	"fmt"
+
+	"math/rand"
+	"time"
+)
+
+var c caching.Cache
+
+func main() {
+
+	// run priodicly cached function
+	for {
+		testCacheFunc()
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func testCacheFunc() {
+	var s string
+
+	// wrap your code you want to cache into DoTry func
+	// and set duration for recaching
+	c.DoTry(time.Second*5, func() {
+
+		s = fmt.Sprint(rand.Int() + 1000)
+		// save new value in cache store
+		c.Save("myvar", s)
+
+	})
+
+	// retrive and print cached value
+	s = c.Get("myvar").(string)
+	fmt.Println("my cached value : ", s)
+
+}
